c0bfef3c1c3fdb629fc9a34b3e35e78c8074b795
marekj <marekj.com@gmail.com>
2009-07-22 18:54:07 -0500

    remove test

--- test/test_helper.rb
+++
@@ -1,9 +0,0 @@
-require 'stringio'
-require 'spec/autorun'
-require File.dirname(__FILE__) + '/../lib/watirloo'
-#Watirloo::BrowserHerd.target = :firefox
-def testfile(filename)
-  path = File.expand_path(File.dirname(__FILE__))
-  uri = "file://#{path}/html/" + filename
-  return uri
-end
