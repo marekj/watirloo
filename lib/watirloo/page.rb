e0c26fc643032bd45e4ccbd715a57fa0da0e2102
marekj <marekj.com@gmail.com>
2009-07-22 18:55:41 -0500

    chagen page from class to module. change face method api

--- lib/watirloo/page.rb
+++ lib/watirloo/page.rb
@@ -1,132 +1,105 @@
 module Watirloo
 
   # Semantic Page Objects Container
-  # Page containes interfaces to Objects of Interest on the Web Page
-  # Each object defined by key, value pair,
-  # Each object defined by key, value pair, 
-  # Keys is a friendly name, recognizable name given by the domain object model.
-  # Some name that is meaningful to the customer.
-  # The value of the interface definiton is a Watir object address in the dom container.
-  # The Page class acts as an Adapter between the Business Domain naming of things and Document Object Model naming of elments.
-  # It strives to adapt Human Readable tests to Machine Executable code
-  class Page
+  # include it in your ClientClass that manages Test. Your client class must provide an instance of browser.
+  # If you don't want an explicit browser the Watirloo.browser will be used.
+  # example
+  #     class UsageScenarioOfSomeFeature
+  #       include Watirloo::Page
+  #     end
+  # now the client GoogleSearch can access browser and elements defined
+  # instead of including it directly in classes that you instantiate to keep track of state you can build modules of pages
+  # that you can later include into your client
+  module Page
 
-    ## Page Eigenclass
-    class << self
-
-      # watir methods are container.method(how, what, value)
-      # if how is optional then value can not be there: 
-      # for example radio_group('nameofradio') # => implicitly this is a :name, 'nameofradio'
-      def make_watir_method(facename, definition) # :nodoc:
-        watirmethod, how, what, value = *definition
-        #log.debug "make_watir_method: #{facename}, watir: #{watirmethod.inspect} how: #{how.inspect}, what: #{what.inspect}, value: #{value.inspect}"
-        if what == nil #if what is nil pass how as what
-          #log.debug "making interface: #{facename} => #{watirmethod}('#{how}')"
-          class_eval "def #{facename}
-                        dombase.#{watirmethod}('#{how}')
-                      end"
-        else
-          extra = value ? ", '#{value}'" : nil # does watir api require a value parameter
-          #log.debug "making interface: #{facename} => #{watirmethod}(:#{how}, '#{what}'#{extra})"
-          class_eval "def #{facename}
-                        dombase.#{watirmethod}(:#{how}, '#{what}'#{extra})
-                      end"
-        end
-      end
-      
-      def make_watir_methods(definitions) # :nodoc:
-        self.faces.update definitions
-        definitions.each_pair do |facename, definition|
-          make_watir_method(facename, definition)
-        end
-      end
-        
-    
-      # Declares Semantic Interface to the DOM elements on the Page 
-      #   face :friendlyname => [watirelement, how, what]
-      # Each interface or face is an object of interest that we want to access by its interface name
-      # example:
-      #   class GoogleSearch < Watirloo::Page
-      #     face :query => [:text_field, :name, 'q]
-      #     face :search => [:button, :name, 'btnG']
-      #  end
-      # each face is a key declared by a semantic symbol that has human meaning in the context of a usecase
-      # each value is an array defining access to Watir [:elementType, how, what]
-      def face(definitions)
-        if definitions.kind_of? Hash
-          make_watir_methods(definitions)
-        else
-          #log.error "Ooops: interface defintion expected to be a Hash, example: face :key => [:text_field, :name, 'name']"
-          raise ::Watir::Exception::WatirException, "Wrong arguments for Page Object definition"
-        end
-      end
-      
-    end # eigenclass
-    
-    attr_accessor :interfaces, :dombase
-    
-    # by convention the Page just attaches to the first available browser.
-    # the smart thing to do is to manage browsers existence on the desktop separately
-    # and supply Page class with the instance of browser you want for your tests.
-    # &block is the convenience at creation time to do some work.
-    # example:
-    #   browser = Watir::start("http://mysitetotest")
-    #   page = Page.new(browser) # specify browser instance to work with or
-    #   page = Page.new # just let the page do lazy thing and attach itself to browser.
-    # part of this page initialization is to provide a convenience while developing tests where
-    # we may have only one browser open and that's the one browser were we want to talk to.
-    # this provides simplicity for those who are just starting with Watirloo
-    def initialize(browser = Watirloo.browser , &blk)
-      @b = browser
-      yield browser if block_given? # allows the shortcut to do some work at page creation
-    end
-    
-    # hold reference to the Watir::Browser
+    # provides access to the browser for a client
     def browser
-      @b
+      @browser ||= Watirloo.browser
     end
-    
-    # hold reference to the Container used as DOM base for our elements.
-    # for example when you work fith frames and the dom you want to act on is in 
-    # browser.frame(framename)
-    # by default domabase it the current browser assuming it does not have any frames
-    def dombase
-      @dombase ||= browser #browser by default
-    end
-          
-    def create_interfaces # :nodoc:
-      @faces = self.class.faces.dup # do not pass reference, only values
-    end
-    
-    # enter values on controls idenfied by keys on the page.
-    # data map is a hash, key represents the page object,
-    # value represents its value to be set, either text, array or boolean
-    def spray(dataMap)
-      dataMap.each_pair do |facename, value|
-        get_face(facename).set value #make every element in the dom respond to set to set its value
-      end
-    end
-    
-    # set values on the page given the interface keys
-    alias set spray 
-  
-    # return Watir object given by its semantic face symbol name
-    def get_face(facename) 
-      if self.respond_to? facename # if there is a defined wrapper method for page element provided
-        return self.send(facename) 
-      else
-        raise ::Watir::Exception::WatirException, 'Unknown Semantic Facename'
-      end
-    end
-    
-    # Delegate execution to browser if no method or face defined on page class
-    def method_missing method, *args
-      if dombase.respond_to?(method.to_sym)
-        return dombase.send(method.to_sym, *args)
-      else
-        raise ::Watir::Exception::WatirException, "Browser does not respond to method: #{method.inspect}"
-      end
-    end
-  end
 
+    # container that delimits the scope of elements.
+    # in a frameless DOM the browser is the doc
+    # however if page with frames you can setup a doc destination to be a frame as the
+    # base container for face accessors
+    def doc
+      browser
+    end
+
+    module ClassMethods
+
+      # "anything which is the forward or world facing part of a system
+      # which has internal structure is considered its 'face', like the facade of a building"
+      # ~  http://en.wikipedia.org/wiki/Face
+      #
+      # Declares Semantic Interface to the DOM elements on the Page (facade) binds a symbol to a block of code that accesses the DOM.
+      # When the user speaks of filling in the last name the are usually entering data in a text_field
+      # we can create a semantic accessor interface like this:
+      #   face(:last_name) {doc.text_field(:name, 'last_nm'}
+      # what matters to the user is on the left (:last_name) and what matters to the programmer is on the right
+      # The face method provides an adapter and insolates the tests form the changes in GUI.
+      #   face(:friendlyname) { watircode } where watircode is actuall way of accessing the element on the page.
+      # Each interface or face is an object of interest that we want to access by its interface name
+      #   example:
+      #
+      #     class GoogleSearch
+      #       include Watirloo::Page
+      #       face(:query)   { doc.text_field(:name, 'q') }
+      #       face(:search)  { doc.button(:name, 'btnG') }
+      #     end
+      #
+      def face(name, *args, &definition)
+        module_eval do
+          define_method(name) do |*args|
+            instance_exec(*args, &definition)
+          end
+        end
+      end
+    end
+
+    # metahook by which ClassMethods become singleton methods of an including module
+    # Perhaps the proper way is to do this
+    # class SomeClass
+    #   include PageHelper
+    #   extend PageHelper::ClassMethods
+    # end
+    # but we are just learning this metaprogramming
+    def self.included(klass)
+      klass.extend(ClassMethods)
+    end
+  
+    # enter values on controls idenfied by keys on the page.
+    # data map is a hash, key represents the page objects that can be filled or set with values,
+    # value represents its value to be set, either text, array or boolean
+    # exmaple: 
+    #     spray :first => "Johnny", :last => 'Begood'
+    #     
+    #     # Given the faces defined
+    #     face(:first) {doc.text_field(:name, 'lst_nm')}
+    #     face(:last) {doc.text_field(:name, 'fst_nm')}
+    def spray(hash)
+      hash.each_pair do |facename, value|
+        send(facename).set value #make every control element in watir respond to set
+      end
+    end
+
+    # set values on the page given the interface keys
+    alias set spray
+
+
+    def scrape(field_keys)
+      data = {}
+      field_keys.each do |k|
+        watir_control = send("#{k}")
+        method_name = case watir_control.class.to_s.split("::").last
+        when "SelectList", "CheckboxGroup", "RadioGroup" then :selected
+        else  
+          :value
+        end
+        data.update k => watir_control.send(method_name)
+      end
+      data
+    end
+
+
+  end
 end
