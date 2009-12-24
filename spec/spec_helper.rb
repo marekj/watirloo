9d7f8740dcf138676af143644a5ce66ff67002d4
marekj <marekj.com@gmail.com>
2009-12-24 10:56:34 -0600

    spec and lib in loadpath strategy. Wrap methods in helper mod

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,20 @@
-require File.dirname(__FILE__) + '/../lib/watirloo'
-require 'stringio'
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'watirloo'
+require 'spec'
 require 'spec/autorun'
 
-def testfile(filename)
-  path = File.expand_path(File.dirname(__FILE__))
-  uri = "file://#{path}/html/" + filename
-  return uri
+Spec::Runner.configure do |config|
+
 end
 
+module WatirlooSpecHelper
+  def testfile(filename)
+    path = File.expand_path(File.dirname(__FILE__))
+    uri = "file://#{path}/html/" + filename
+    return uri
+  end
+end
+
+include WatirlooSpecHelper
 
